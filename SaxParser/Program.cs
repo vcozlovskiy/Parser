645ee820d1f907beedee1341abffdbdbdf4f5c50
Vlad Kozlovskiy <kozlovskii9928@gmail.com>
2021-12-05 13:51:02 +0300

    first commit

---
+++ SaxParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaxParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
